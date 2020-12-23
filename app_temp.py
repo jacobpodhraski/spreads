f20b7a459bdb1b12f657ecb6f607fcb12f218efd
Jacob Podhraski <jacob.podhraski@l3harris.com>
2020-12-22 19:46:39 -0500

    Displayed all data on website in tabular format. Need to add in the form next

---
+++ app_temp.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/spreadhistory"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique = True)
+    email = db.Column(db.String(120), unique = True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
