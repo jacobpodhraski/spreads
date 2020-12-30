0201fc79eb39a80b50f6092d0720443cccb2d513
Jacob Podhraski <jacob.podhraski@l3harris.com>
2020-12-30 17:14:39 -0500

    Added year by year data (2010-2020) in db. Added new year to year selector

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
-nfl2020 = db.Table('nfl2020a', db.metadata, autoload = True, autoload_with=db.engine)
+nfl2020 = db.Table('nfl2020', db.metadata, autoload = True, autoload_with=db.engine)
 
 @app.route('/')
 def showQueryResults():
@@ -25,12 +25,26 @@ def showQueryResults():
 @app.route('/new', methods = ['GET', 'POST'])
 def query():
 
-    print("Test")
     if request.method == 'POST':
-        team = request.form['team']
-        isFav = request.form['isFav']
+
+        team      = request.form['team']
+        isFav     = request.form['isFav']
         isGreater = request.form['isGreater']
-        points = request.form['points']
+        points    = request.form['points']
+        beginYear = request.form['beginYear']
+        endYear   = request.form['endYear']
+
+        # Check if the years are valid
+        if int(endYear) - int(beginYear) < 0:
+            return render_template('new.html', errorMessage = "Years are invalid")
+
+
+        yearByYear = OrderedDict()
+        for year in range(int(beginYear), int(endYear) + 1):
+            if team != 'any':
+                queryStatement = prepareQueryStatementForTeam(str(year), team, isFav, isGreater, points)
+
+
 
         teamEntries = findAllGamesForTeam(team, isFav, isGreater, points)
 
@@ -52,9 +66,34 @@ def query():
 
         return render_template('new.html')
 
+def prepareQueryStatementForTeam(year, team, isFav, isGreater, points):
+
+    statement = "select * from nfl" + year + " where team like '"+ team + "' and "
+
+    if isFav:
+        if isGreater:
+            # Set the spread requirement to under 30 (lowest o.u in history)
+            statement.append('close < 30 and close > ' + points + ';'
+        else:
+            statement.append('close > 0 and close < ' + points + ';'
+
+        favGameIds = findAllGameIds(db.engine.execute(statement))
+
+    else:
+        statement.append('close >= 30')
+        obtainAllUnderdogs = db.execute(statement)
+        underdogGameIds = findAllGameIds(obtainAllUnderdogs)
+        relevantUnderdogGames =
+
+
+        if isGreater:
+
+        else:
+
+
 def findAllGamesForTeam(team, isFav, isGreater, points):
 
-    queryStatement = 'select * from nfl2020a'
+    queryStatement = 'select * from nfl2020'
     if team != 'any':
         queryStatement = queryStatement + " where team like '" + team + "';"
     else:
@@ -72,53 +111,16 @@ def findAllGameIds(teamEntries):
 
     return listOfGameIds
 
-def getTableOfAllRelevantGames(gameIds, team, isFav, isGreater, points):
+def getTableOfAllRelevantGames(gameIds, year, team):
 
-    queryStatement = "select * from nfl2020a where "
-
-
-
+    queryStatement = "select * from nfl" + year + " where "
     for i in range(0, len(gameIds)):
-
-        currentGame =  db.engine.execute("select * from nfl2020a where gameid = " + str(gameIds[i]))
-
-        gameDictForComparsion = OrderedDict()
-
-        for row in currentGame:
-            continue
-
-
+        if i == 0:
+            queryStatement.append("gameid = " + str(gameIds[i] + " ")
+        else:
+            queryStatement.append("or gameid = " + str(gameIds[i]) + " ")
+    queryStatement.append(";")
     return db.engine.execute(queryStatement)
 
-def prepareGameLinesDict(currentGames):
-
-    currentGameId = 0
-    previousGameId = 0
-    gameScores = OrderedDict()
-
-    for row in currentGames:
-
-        currentGameId = row.gameid
-
-        if previousGameId == currentGameId:
-            homeScore = row.final
-            previousGameId = currentGameId
-
-            openHome = row.open
-            closeHome = row.close
-            gameScores[row.gameid] = [vistorScore, homeScore, openVis, openHome, closeVis, closeHome]
-
-        else:
-            vistorScore = row.final
-            openVis = row.open
-            closeVis = row.close
-            previousGameId = currentGameId
-
-    print(gameScores)
-    return gameScores
-
-def obtainRecords(scoresDict):
-
-    return [1, 2]
 if __name__ == '__main__':
     app.run(debug = True)
