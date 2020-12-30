b76233df8d4e6181464ca3ab982460bac6871344
Jacob Podhraski <jacob.podhraski@l3harris.com>
2020-12-29 21:44:47 -0500

    Added 10 years of data to the database, added months selection and years

--- addGameIndex.py
+++ addGameIndex.py
@@ -16,18 +16,22 @@ def createListOfGameIds(numRows):
 
 if __name__ == '__main__':
 
-    data = pd.read_csv('datasets/nfl odds 2020-21.csv', header=None)
-    print (data)
-    numRows = data.shape[0]
-    print('Num Rows: ' + str(numRows))
-    idsCol = createListOfGameIds(numRows)
-    print('len ids: ' + str(len(idsCol)))
+    years = ["2019-20", "2018-19", "2017-18", "2016-17", "2015-16", "2014-15", "2013-14", "2012-13", "2011-12", "2010-11"]
 
-    data['Game ID'] = idsCol
+    for i in range(0,len(years)):
 
-    print(data)
+        data = pd.read_csv('datasets/nfl odds ' + years[i] + '.csv')
+        print (data)
+        numRows = data.shape[0]
+        print('Num Rows: ' + str(numRows))
+        idsCol = createListOfGameIds(numRows)
+        print('len ids: ' + str(len(idsCol)))
 
-    data.to_csv('datasets/nfl odds 2020-21 idx.csv', header=None, index=False)
+        data['Game ID'] = idsCol
+
+        print(data)
+
+        data.to_csv('datasets/nfl odds ' + years[i] + 'idx.csv', header=None, index=False)
 
 
 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from collections import OrderedDict
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/spreadhistory'
@@ -23,25 +24,33 @@ def showQueryResults():
 
 @app.route('/new', methods = ['GET', 'POST'])
 def query():
+
+    print("Test")
     if request.method == 'POST':
         team = request.form['team']
         isFav = request.form['isFav']
         isGreater = request.form['isGreater']
         points = request.form['points']
 
-        print (team + isFav + isGreater + points)
-
-        r = db.engine.execute('select ' + 'team' + ', final from nfl2020')
-
         teamEntries = findAllGamesForTeam(team, isFav, isGreater, points)
 
         gameIds = findAllGameIds(teamEntries)
         print(gameIds)
 
-        currentGamesTable = getTableOfAllRelevantGames(gameIds)
+        #currentGamesTable = getTableOfAllRelevantGames(gameIds)
 
-        return render_template('new.html', games = currentGamesTable )
+        #dictOfGameScores = prepareGameLinesDict(currentGamesTable)
 
+       # [suRecord, atsRecord] = obtainRecords(dictOfGameScores)
+
+        return render_template('new.html')
+
+       # return render_template('new.html', games = getTableOfAllRelevantGames(gameIds))
+
+
+    else:
+
+        return render_template('new.html')
 
 def findAllGamesForTeam(team, isFav, isGreater, points):
 
@@ -63,20 +72,53 @@ def findAllGameIds(teamEntries):
 
     return listOfGameIds
 
-def getTableOfAllRelevantGames(gameIds):
+def getTableOfAllRelevantGames(gameIds, team, isFav, isGreater, points):
 
     queryStatement = "select * from nfl2020a where "
 
+
+
     for i in range(0, len(gameIds)):
 
-        if i == 0:
-            queryStatement = queryStatement + 'gameid = ' + str(gameIds[i])
-        else:
-            queryStatement = queryStatement + ' or gameid = ' + str(gameIds[i])
+        currentGame =  db.engine.execute("select * from nfl2020a where gameid = " + str(gameIds[i]))
+
+        gameDictForComparsion = OrderedDict()
+
+        for row in currentGame:
+            continue
 
 
     return db.engine.execute(queryStatement)
 
+def prepareGameLinesDict(currentGames):
 
+    currentGameId = 0
+    previousGameId = 0
+    gameScores = OrderedDict()
+
+    for row in currentGames:
+
+        currentGameId = row.gameid
+
+        if previousGameId == currentGameId:
+            homeScore = row.final
+            previousGameId = currentGameId
+
+            openHome = row.open
+            closeHome = row.close
+            gameScores[row.gameid] = [vistorScore, homeScore, openVis, openHome, closeVis, closeHome]
+
+        else:
+            vistorScore = row.final
+            openVis = row.open
+            closeVis = row.close
+            previousGameId = currentGameId
+
+    print(gameScores)
+    return gameScores
+
+def obtainRecords(scoresDict):
+
+    return [1, 2]
 if __name__ == '__main__':
     app.run(debug = True)
